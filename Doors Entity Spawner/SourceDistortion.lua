4cad3e564777ac6016b974612cc8a5483cd292ec
Isth4t <116909814+Isth4t@users.noreply.github.com>
2022-12-24 12:10:19 +0200

    Update SourceDistortion.lua

--- Doors Entity Spawner/SourceDistortion.lua
+++ Doors Entity Spawner/SourceDistortion.lua
@@ -419,7 +419,7 @@ Spawner.runJumpscare = function(config)
     Face.Image = image2
     sound2:Play()
     
-    TS:Create(Face, TweenInfo.new(3), {Size = UDim2.new(0, absHeight * 7, 0,  absHeight * 7), ImageTransparency = 0.5}):Play()
+    TS:Create(Face, TweenInfo.new(3), {Size = UDim2.new(0, absHeight * 2, 0,  absHeight * 2), ImageTransparency = 0.5}):Play()
     task.wait(3)
     JumpscareGui:Destroy()
     
